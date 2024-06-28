2372032453f7cef2871a6c3c6a78cc3d2252f399
Fadi Alnajar <fadi.alnajar20@gmail.com>
2024-06-28 17:16:15 +0300

    Solve the problem Duplicates in Array

--- Challenges/Find-Duplicates/Program.cs
+++ Challenges/Find-Duplicates/Program.cs
@@ -51,7 +51,7 @@ namespace challenges_and_data_structures
             {
                 Console.Write(key + " ");
             }
-            Console.WriteLine();
+            //Console.WriteLine();
         }
         static void Main(string[] args)
         {
@@ -94,17 +94,35 @@ namespace challenges_and_data_structures
             //Print(result3);
 
             // Middle Value
-            int[] testArray1 = { 1, 2, 3, 4 };
-            int[] result1 = MiddleValue(testArray1);
-            Print(result1);  // Output: [1, 2, 5, 3, 4]
+            //int[] testArray1 = { 1, 2, 3, 4 };
+            //int[] result1 = MiddleValue(testArray1, 5);
+            //Print(result1);  // Output: [1, 2, 5, 3, 4]
 
-            int[] testArray2 = { 10, 20, 30, 40, 50 };
-            int[] result2 = MiddleValue(testArray2);
-            Print(result2);  // Output: [10, 20, 30, 25, 40, 50]
+            //int[] testArray2 = { 10, 20, 30, 40, 50 };
+            //int[] result2 = MiddleValue(testArray2, 25);
+            //Print(result2);  // Output: [10, 20, 30, 25, 40, 50]
 
-            int[] testArray3 = { 7, 14, 21, 28 };
-            int[] result3 = MiddleValue(testArray3);
-            Print(result3);  // Output: [7, 14, 10, 21, 28]
+            //int[] testArray3 = { 7, 14, 21, 28 };
+            //int[] result3 = MiddleValue(testArray3, 10);
+            //Print(result3);  // Output: [7, 14, 10, 21, 28]
+
+
+            int[] testArray1 = { 1, 2, 3, 1, 2, 3 };
+            int[] testArray2 = { 16, 8, 31, 17, 15, 23, 17, 8 };
+            int[] testArray3 = { 5, 10, 16, 20, 10, 16 };
+
+            //Console.WriteLine("Test Array 2: " + string.Join(" ", FindDuplicates(testArray2)));
+            //Console.WriteLine("Test Array 3: " + string.Join(" ", FindDuplicates(testArray3)));
+
+            Print(testArray1);
+            Console.Write(" --> ");
+            Console.Write(string.Join(" ", FindDuplicates(testArray1)) + "\n");
+            Print(testArray2);
+            Console.Write(" --> ");
+            Console.Write(string.Join(" ", FindDuplicates(testArray2)) + "\n");
+            Print(testArray3);
+            Console.Write(" --> ");
+            Console.Write(string.Join(" ", FindDuplicates(testArray3)));
 
             Console.ReadKey();
         }
@@ -152,6 +170,28 @@ namespace challenges_and_data_structures
             return modifiedArray;
         }
 
+            public static int[] FindDuplicates(int[] nums)
+            {
+                Dictionary<int, int> directory = new Dictionary<int, int>();
+
+                foreach (int num in nums)
+                {
+                    if (directory.ContainsKey(num))
+                        directory[num]++;
+                    else
+                        directory.Add(num, 1);
+                }
+
+                List<int> storDuplicates = new List<int>();
+
+                foreach (var kvp in directory)
+                {
+                    if (kvp.Value > 1)
+                    storDuplicates.Add(kvp.Key);
+                }
+
+                return storDuplicates.ToArray();
+            }
 
     }
 }
