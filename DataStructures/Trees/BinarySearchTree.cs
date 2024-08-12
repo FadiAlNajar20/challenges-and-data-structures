22256878297df00144e7fcf87d3aa6330ac95448
Fadi Alnajar <fadi.alnajar20@gmail.com>
2024-08-12 21:48:32 +0300

    Create Binary Tree and Binary Search Tree Implementation

---
+++ DataStructures/Trees/BinarySearchTree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenges_and_data_structures
+{
+    public class BinarySearchTree : BinaryTree
+    {
+        public void Add(int data)
+        {
+            Root = AddRecursive(Root, data);
+        }
+
+        private Node AddRecursive(Node node, int data)
+        {
+            if (node == null)
+            {
+                return new Node(data);
+            }
+
+            if (data < node.Value)
+            {
+                node.Left = AddRecursive(node.Left, data);
+            }
+            else if (data > node.Value)
+            {
+                node.Right = AddRecursive(node.Right, data);
+            }
+
+            return node;
+        }
+
+        public bool Contains(int data)
+        {
+            return ContainsRecursive(Root, data);
+        }
+
+        private bool ContainsRecursive(Node node, int data)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (data == node.Value)
+            {
+                return true;
+            }
+
+            return data < node.Value ? ContainsRecursive(node.Left, data) : ContainsRecursive(node.Right, data);
+        }
+
+        public void Remove(int data)
+        {
+            Root = RemoveRecursive(Root, data);
+        }
+
+        private Node RemoveRecursive(Node node, int data)
+        {
+            if (node == null) return null;
+
+            if (data < node.Value)
+            {
+                node.Left = RemoveRecursive(node.Left, data);
+            }
+            else if (data > node.Value)
+            {
+                node.Right = RemoveRecursive(node.Right, data);
+            }
+            else
+            {
+                if (node.Left == null) return node.Right;
+                if (node.Right == null) return node.Left;
+
+                node.Value = MinValue(node.Right);
+                node.Right = RemoveRecursive(node.Right, node.Value);
+            }
+            return node;
+        }
+
+        private int MinValue(Node node)
+        {
+            int minv = node.Value;
+            while (node.Left != null)
+            {
+                minv = node.Left.Value;
+                node = node.Left;
+            }
+            return minv;
+        }
+    }
+
+}
---
+++ DataStructures/Trees/BinaryTree.cs
@@ -0,0 +1,49 @@
+﻿namespace challenges_and_data_structures
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        // PreOrder Traversal (Root, Left, Right)
+        public void PreOrder(Node node, Action<int> callback)
+        {
+            if (node == null) return;
+            callback(node.Value);
+            PreOrder(node.Left, callback);
+            PreOrder(node.Right, callback);
+        }
+
+        // InOrder Traversal (Left, Root, Right)
+        public void InOrder(Node node, Action<int> callback)
+        {
+            if (node == null) return;
+            InOrder(node.Left, callback);
+            callback(node.Value);
+            InOrder(node.Right, callback);
+        }
+
+        // PostOrder Traversal (Left, Right, Root)
+        public void PostOrder(Node node, Action<int> callback)
+        {
+            if (node == null) return;
+            PostOrder(node.Left, callback);
+            PostOrder(node.Right, callback);
+            callback(node.Value);
+        }
+
+        // Print Tree
+        public void Print(Node node, string indent = "", bool isLeft = true)
+        {
+            if (node == null)
+            {
+                Console.WriteLine($"{indent}*");
+                return;
+            }
+
+            Console.WriteLine($"{indent}{(isLeft ? "├──" : "└──")}{node.Value}");
+            indent += isLeft ? "│   " : "    ";
+            Print(node.Left, indent, true);
+            Print(node.Right, indent, false);
+        }
+    }
+}
